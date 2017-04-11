13ba957611da359935cf68332351df58ac85e09c
akumar36 <akumar36@tcsl0758fbe88.corp.intuit.net>
2017-04-11 12:23:12 -0700

    uses form-login

--- website-tracker/src/main/java/inst/an/websitetracker/security/WebSecurityConfiguration.java
+++ website-tracker/src/main/java/inst/an/websitetracker/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -16,18 +17,18 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/js/**", "/bootstrap/css/**").permitAll()
                 .anyRequest().authenticated()
             .and()
-            	.formLogin()
-                .loginPage("/login")
-                .permitAll()
+            	.formLogin().permitAll()
+            	.and()
+    			.httpBasic()
             .and()
-                .httpBasic()
-            .and()
-                .logout()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .permitAll();
+        
+        http.csrf().disable();
     }
     
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
