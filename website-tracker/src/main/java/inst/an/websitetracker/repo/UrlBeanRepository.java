013fe653d15f315acbea388a8da2556b4c123579
akumar36 <akumar36@tcsl0758fbe88.corp.intuit.net>
2017-04-12 11:30:36 -0700

    more improvements

--- website-tracker/src/main/java/inst/an/websitetracker/repo/UrlBeanRepository.java
+++ website-tracker/src/main/java/inst/an/websitetracker/repo/UrlBeanRepository.java
@@ -10,7 +10,8 @@ import inst.an.websitetracker.beans.UrlBean;
 
 public interface UrlBeanRepository extends JpaRepository<UrlBean, Long>{
 	//@Query(value="SELECT * FROM TWEET where tweeter_id in (SELECT u.id FROM user_profile u LEFT JOIN connection c WHERE c.followed_by_id = u.id AND c.follower_id = :userId)", nativeQuery=true)
-	@Query(value="select * from (SELECT domain, count(*) as hits FROM url_bean group by domain order by hits desc) where rownum() < 4", nativeQuery=true)
+//	@Query(value="select * from (SELECT domain, count(*) as hits FROM url_bean group by domain order by hits desc) where rownum() < 4", nativeQuery=true)
+	@Query(value="SELECT domain, count(*) as hits FROM url_bean group by domain having count(*) in (select * from (SELECT distinct count(*) as hits FROM url_bean group by domain) where rownum() < 4) order by hits desc", nativeQuery=true)
 	List<Object[]> findTop3Visited();
 
 	UrlBean findByDomain(String domain);
--- website-tracker/src/test/java/inst/an/UrlControllerTest.java
+++ website-tracker/src/test/java/inst/an/UrlControllerTest.java
@@ -1,23 +1,16 @@
 package inst.an;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import inst.an.websitetracker.WebsiteTrackerApplication;
 import inst.an.websitetracker.beans.UrlBean;
@@ -27,20 +20,8 @@ import inst.an.websitetracker.beans.UrlBean;
 @WebAppConfiguration
 
 public class UrlControllerTest {
-	
-	  @Autowired
-	  private WebApplicationContext context;
-
-	  private MockMvc mvc;
 	  
 	  private static final String BASE_URL = "http://localhost:8080";
-
-	  @Before
-	  public void setup() {
-	      mvc = MockMvcBuilders
-	              .webAppContextSetup(context)
-	              .build();
-	  }
 	  
 	  @Test
 	  public void testLogin() throws Exception{
@@ -51,7 +32,7 @@ public class UrlControllerTest {
 			
 			assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
 			
-			response = testRestTemplate.withBasicAuth("ananth", "ananth").getForEntity(BASE_URL+"/urls/1", UrlBean.class);
+			response = testRestTemplate.withBasicAuth("user", "password").getForEntity(BASE_URL+"/urls/1", UrlBean.class);
 			
 			assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
 	  }
@@ -64,12 +45,12 @@ public class UrlControllerTest {
 			url.setUrl("https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html");
 			
 			ResponseEntity<UrlBean> response = testRestTemplate
-					.withBasicAuth("ananth", "ananth")
+					.withBasicAuth("user", "password")
 					.postForEntity(BASE_URL+"/urls/", url, UrlBean.class);
 			
 			assertThat(response.getStatusCode(), equalTo(HttpStatus.CREATED));
 			
-			ResponseEntity<String> responseStr = testRestTemplate.withBasicAuth("ananth", "ananth").getForEntity(response.getHeaders().getLocation(), String.class);
+			ResponseEntity<String> responseStr = testRestTemplate.withBasicAuth("user", "password").getForEntity(response.getHeaders().getLocation(), String.class);
 			
 			assertThat(responseStr.getStatusCode(), equalTo(HttpStatus.OK));
 			
@@ -79,14 +60,21 @@ public class UrlControllerTest {
 	  @Test
 	  public void testGetTop3() throws Exception{
 
-			TestRestTemplate testRestTemplate = new TestRestTemplate();
+		  	TestRestTemplate testRestTemplate = new TestRestTemplate();
 			UrlBean url = new UrlBean();
 			url.setUrl("https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html");
 			
 			ResponseEntity<UrlBean> response = testRestTemplate
-					.withBasicAuth("ananth", "ananth")
-					.postForEntity(BASE_URL+"/urls/top3", url, UrlBean.class);
+					.withBasicAuth("user", "password")
+					.postForEntity(BASE_URL+"/urls/", url, UrlBean.class);
 			
 			assertThat(response.getStatusCode(), equalTo(HttpStatus.CREATED));
+			
+			
+			ResponseEntity<String> response2 = testRestTemplate
+					.withBasicAuth("user", "password")
+					.getForEntity(BASE_URL+"/urls/top3", String.class);
+			
+			assertThat(response2.getStatusCode(), equalTo(HttpStatus.OK));
 	  }	  
 }
