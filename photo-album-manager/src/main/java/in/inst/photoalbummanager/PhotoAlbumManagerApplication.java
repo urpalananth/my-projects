16a175891c560b5fdc5b9176f7b03e85b8edb1d5
akumar36 <akumar36@172.28.170.1>
2017-05-12 14:28:27 -0700

    photo-album-manager initial check-in

---
+++ photo-album-manager/src/main/java/in/inst/photoalbummanager/PhotoAlbumManagerApplication.java
@@ -0,0 +1,12 @@
+package in.inst.photoalbummanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAlbumManagerApplication.class, args);
+	}
+}
---
+++ photo-album-manager/src/test/java/in/inst/photoalbummanager/PhotoAlbumManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package in.inst.photoalbummanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoAlbumManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
